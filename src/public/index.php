5d6dc4d21aa9970f6695e0278a38f303ca3083f0
Administrateur <Administrateur@WIN-7RPTUBDKC86>
2014-07-08 15:37:28 +0200

    ADD MASS TRUCS!!!

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,5 @@
 <?php
-
+// CONSTANTE ZEND
 define('DS', DIRECTORY_SEPARATOR);
 define('APP_ENV', getenv('APPLICATION_ENV') ?: 'production');
 define('ROOT_PATH', dirname(dirname(__DIR__)));
@@ -8,6 +8,9 @@ define('LIB_PATH', ROOT_PATH.DS.'library');
 define('PUBLIC_PATH', SRC_PATH.DS.'public');
 define('VENDOR_PATH', ROOT_PATH.DS.'vendor');
 define('APP_PATH', SRC_PATH.DS.'application');
+
+//CONSTANTE APPLI
+define('APP_NAME', "Zf-ORM-git");
 
 //require_once LIB_PATH.DS."sf2boss".DS."sflib".DS."Error.php";
 require_once VENDOR_PATH.DS.'autoload.php';
